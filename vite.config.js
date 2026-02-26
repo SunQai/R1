6d86bc4b7315f93729737056e2542b9c6ac43e1f
SunQai <109790424+SunQai@users.noreply.github.com>
2026-02-26 23:03:25 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/R1/', // ВАЖНО!!!
   plugins: [react()],
+  base: '/R1/',
 })
+
